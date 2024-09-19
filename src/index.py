1ce2e8068bc6547900ade8a798fb503c05d3882c
pavel <pavel@pavels-iMac.local>
2024-09-19 04:05:01 -0700

    Добавил папку с процессами

--- src/index.py
+++ src/index.py
@@ -7,7 +7,8 @@ import settings
 import json
 import random
 import string
-from player import Player
+from user import User
+import processes
 
 
 # Инициализация бота
@@ -23,8 +24,8 @@ tags = {"Игры": 0, "Деньги": 0}
 @tgbot.message_handler(commands=['start'])
 def start(message):
     username = message.from_user.username
-    if username not in users:
-        users[username] = Player(message.chat.id, username)
+    if username not in users: #ЗАПРОС К БД
+        users[username] = User(message.chat.id, username) #ЗАПРОС К БД (На добавление)
 
         tgbot.send_message(message.chat.id, f"Добро пожаловать, {username}!")
     tgbot.delete_message(message.chat.id, message.message_id)
@@ -34,62 +35,71 @@ def start(message):
 # _________________МЕНЮ_________________
 
 def tag_select(call):
-    try:
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        for tag in tags.keys():
-            if tag in users[call.from_user.username].tags:
-                markup.add(types.InlineKeyboardButton("✅ " + tag, callback_data=f"tag_to_user_remove {tag}"))
-            else:
-                markup.add(types.InlineKeyboardButton(tag, callback_data=f"tag_to_user_add {tag}"))
+    #try:
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    for tag in tags.keys():
+        if tag in users[call.from_user.username].tags: #ЗАПРОС К БД
+            markup.add(types.InlineKeyboardButton("✅ " + tag, callback_data=f"tag_to_user_remove {tag}"))
+        else:
+            markup.add(types.InlineKeyboardButton(tag, callback_data=f"tag_to_user_add {tag}"))
 
-        markup.add(types.InlineKeyboardButton("Готово", callback_data=f"to_main_menu"))
-    except Exception as e:
-        print(e)
+    markup.add(types.InlineKeyboardButton("Готово", callback_data=f"to_main_menu"))
+    #except Exception as e:
+        #print(e)
     try:
         tgbot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f"Что вас интересует?", reply_markup=markup)
     except:
         main = tgbot.send_message(call.chat.id, f"Что вас интересует?", reply_markup=markup)
 
 def main_menu(call):
-    try:
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        market = types.InlineKeyboardButton("🚀 Создать", callback_data="create_event")
-        wallet = types.InlineKeyboardButton("📅 Календарь", callback_data="calendar")
-        markup.add(market, wallet)
-    except Exception as e:
-        print(e)
+    #try:
+    markup = types.InlineKeyboardMarkup(row_width=2)
+    market = types.InlineKeyboardButton("🚀 Создать", callback_data="create_event")
+    wallet = types.InlineKeyboardButton("📅 Календарь", callback_data="calendar")
+    markup.add(market, wallet)
+                                                                                                                                                # Нереализованный функционал Веб-приложения
+                                                                                                                                                #web_app_info = types.WebAppInfo("https://localhost:5000")
+                                                                                                                                                #button = types.KeyboardButton(text="Веб-приложение", web_app=web_app_info)
+                                                                                                                                                #markup.add(button)
+    #except Exception as e:
+        #print(e)
     try:
         tgbot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=f"Главное меню", reply_markup=markup)
     except:
-        main = tgbot.send_message(call.chat.id, f"Главное меню", reply_markup=markup)
+        main = tgbot.send_message(call.message.chat.id, f"Главное меню", reply_markup=markup)
 
 
 # ______________________________________
 
 @tgbot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
-    try:
-        if call.data.split(" ", 1)[0] == "tag_to_user_add":
-            username = call.from_user.username
-            users[username].tags.append(call.data.split(" ")[1])
-            tag_select(call)
-        
-        elif call.data.split(" ", 1)[0] == "tag_to_user_remove":
-            username = call.from_user.username
-            users[username].tags.remove(call.data.split(" ")[1])
-            tag_select(call)
-        
-        elif call.data.split(" ", 1)[0] == "to_main_menu":
-            main_menu(call)
+    #try:
+    if call.data.split(" ", 1)[0] == "tag_to_user_add":
+        username = call.from_user.username
+        users[username].tags.append(call.data.split(" ")[1]) #ЗАПРОС К БД
+        tag_select(call)
+    
+    elif call.data.split(" ", 1)[0] == "tag_to_user_remove":
+        username = call.from_user.username
+        users[username].tags.remove(call.data.split(" ")[1]) #ЗАПРОС К БД
+        tag_select(call)
+    
+    elif call.data.split(" ", 1)[0] == "to_main_menu":
+        main_menu(call)
 
-        else:
-            tgbot.answer_callback_query(callback_query_id=call.id, text="Для авторизации напишите /start", show_alert=True)
+    elif call.data == "create_event":
+        tgbot.register_next_step_handler(msg, create_event, tgbot, call)
 
-
-    except Exception as e:
+    else:
         tgbot.answer_callback_query(callback_query_id=call.id, text="Для авторизации напишите /start", show_alert=True)
-        print(e)
 
+
+    #except Exception as e:
+    #    tgbot.answer_callback_query(callback_query_id=call.id, text="Для авторизации напишите /start", show_alert=True)
+    #    print(e)
+
+def process_create_event(call):
+    #Процесс создания
 
 
 if __name__ == "__main__":
--- src/webapp/app.py
+++ src/webapp/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+import index
 from player import Player
 
 app = Flask(__name__)
@@ -6,33 +7,16 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     username = "User"  # Замените на реальное имя пользователя
-    items = {
-        "Ключ 1": "Значение 1",
-        "Ключ 2": "Значение 2",
-        "Ключ 3": "Значение 3",
-        "Клюdч 1": "Значение 1",
-        "Ключd 2": "Значение 2",
-        "Ключv 3": "Значение 3",
-        "Ключv 1": "Значение 1",
-        "Клю ч 2": "Значение 2",
-        "Ключ 3": "Значение 3",
-        "Ключz 1": "Значение 1",
-        "Клюxч 2": "Значение 2",
-        "Ключf 3": "Значение 3",
-        "Ключf 1": "Значение 1",
-        "Ключ e2": "Значение 2",
-        "Ключ 3q": "Значение 3",
-        "gКлюч 1": "Значение 1",
-        "Кvлюч 2": "Значение 2",
-        "Клwюч 3": "Значение 3",
-        "Клюyч 1": "Значение 1",
-        "Ключw 2": "Значение 2",
-        "Ключ j3": "Значение 3",
-        "Ключ 1a": "Значение 1",
-        "gКлюч 2": "Значение 2",
-        "Кsлюч 3": "Значение 3"
-    }  # Днамический список с ключами и значениями
+    items = tags # Днамический список с ключами и значениями
     return render_template('index.html', username=username, items=items)
+
+@app.route('/auth', methods=['POST'])
+def webapp():
+    data = request.json
+    user_id = data.get('user_id')
+    username = data.get('username')
+    # Обработка данных пользователя
+    return jsonify({"status": "success", "user_id": user_id, "username": username})
 
 @app.route('/button-click/<key>', methods=['POST'])
 def button_click(key):
