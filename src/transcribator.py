7afafeed3be89b33b82f66896a8530196d1cbe81
Yaro <yarillo@ngs.ru>
2024-09-21 10:15:43 +0600

    edit transcribator

--- src/transcribator.py
+++ src/transcribator.py
@@ -1,94 +1,132 @@
 import speech_recognition as sr
 from dateutil import parser
-from datetime import datetime
+import datetime
+from transformers import pipeline
 import subprocess
-
-'''
-ВНИМАНИЕ!!! Тут все сделано максимально костыльно лишь для того,
-чтобы показать возможность и функциональность голосового управления.
-В случае согласования такого подхода это все будет переделано:
-
-- для транскрибации будет использоваться локальная модель (openAI Whisper large),
-чтобы аудио не уходило "налево" и для ускорения обработки;
-
-- для классификации типа обращения (запрос создания события, запрос удаления события,
-показ календаря и т.п.) будет использоваться специально созданная и обученная на
-пользовательских данных модель на архитектуре transformers;
-
-- для поиска именованных сущностей в тексте запроса (тип события, дата, время и т.п.)
-предполагается к использованию языковая модель NER (библиотека Spacy), также дообученная
-на пользовательских данных;
-
-- возможно, и для классификации, и для NER будет использоваться одна и та же модель на
-базе transformers;
-
-- также допускается, что для этих целей будет задействована коммерческая большая
-языковая модель-чатбот (вероятнее всего, gigachat от Сбера);
-
-Так как создание моделей это ОЧЕНЬ трудоемкий процесс, выполнить это за три дня
-не представилось возможным, поэтому демонстрируется лишь общая концепция.
-'''
 
 
 class Transcribator:
     def __init__(self) -> None:
         self.rec = sr.Recognizer()
+        self.qa_model = pipeline("question-answering", "timpal0l/mdeberta-v3-base-squad2")
     
+
     def transcribe(self, audiofile) -> list:
         with sr.AudioFile(audiofile) as source:
             audio = self.rec.record(source)
-            
+            try:
+                text = self.rec.recognize_google(audio, language="ru-RU")
+                return text
+            except Exception:
+                return ['Error']
+
+
+    def request_preprocessing(self, text: str) -> list:
+        '''Обработка и формализация пользовательского запроса'''
         try:
-            text = self.rec.recognize_google(audio, language="ru-RU")
-            return text.split()
-        
-        except sr.UnknownValueError:
-            print("Error: could not understand audio")
-            return ['Error']
-        except sr.RequestError as e:
-            print("Error; {0}".format(e))
-            return ['Error']
-    
-    def events_type_recognize(self, text: list) -> dict:
-        event = {}
-        if "покажи" in text:
-            if "сегодня" in text:
-                event['datetime'] = datetime.now()
-            if "неделю" in text:
-                event['datetime'] = datetime.now()
-            if "месяц" in text:
-                event['datetime'] = datetime.now()
-        elif "запланируй" in text:
-            event_datetime = self.keywords_finder(text=text)
-            event_datetime = parser.parse(f"{event_datetime['month']} {event_datetime['day']} {event_datetime['year']} {event_datetime['time']}")
-            event['event_type'] = text[text.index("запланируй") + 1]
-            event['place'] = text[text.index("место") + 1]  
-            event['datetime'] = event_datetime
-            return event
-        elif "отмени" in text:
-            event_datetime = self.keywords_finder(text=text)
-            event_datetime = parser.parse(f"{event_datetime['month']} {event_datetime['day']} {event_datetime['year']} {event_datetime['time']}")
-            event['event_type'] = text[text.index("отмени") + 1]
-            event['place'] = text[text.index("место") + 1]
-            event['datetime'] = event_datetime
-            return event
+            datetime = self.parse_date(text=text)
+        except:
+            return ['error']
+        duraton = self.parse_duration(text=text)
+        place = self.parse_place(text=text)
+        e_type = self.parse_event_type(text=text)
+        event_type_answer = self.qa_model(question="Что нужно сделать?", context=text)['answer'].strip()
+        plane_sinonyms = ["забронир", "созд", "запланир", "организ", "сдел"]
+        for sinonim in plane_sinonyms:
+            if sinonim in event_type_answer:
+                return ['add', datetime, place, duraton, e_type]
+        rm_sinonyms = ["удал", "убер", "убр", "отмен", "очист"]
+        for sinonim in rm_sinonyms:
+            if sinonim in event_type_answer:
+                return ['remove', datetime, place]
+        show_events_sinonyms = ["покаж", "посм", "вывед", "какие", "планы", "календар"]
+        for sinonim in show_events_sinonyms:
+            if sinonim in event_type_answer:
+                return ['search', datetime]
+        return ['error']
 
-    def keywords_finder(self, text: list) -> dict:
-        keywords = {}
-        keywords['time'] = text[text.index("время") + 1]
-        keywords['day'] = text[text.index("дата") + 1]
-        keywords['month'] = text[text.index("дата") + 2]
-        keywords['month'] = self.transform_month(keywords['month'])
-        keywords['year'] = text[text.index("дата") + 3]
-        return keywords
 
-    def transform_month(self, month: str) -> str:
+    def parse_place(self, text: str) -> str:
+        '''Поиск указания локации в запросе'''
+        place = self.qa_model(question="О каком месте идет речь?", context=text)['answer'].strip()
+        if "переговор" in place:
+            return "комната для переговоров"
+        elif "зал" in place:
+            return "большой зал"
+        else:
+            return "комната для переговоров"
+
+
+    def parse_event_type(self, text: str) -> str:
+        '''Поиск указания типа встречи в запросе'''
+        event_type = self.qa_model(question = "О каком событии идет речь?", context=text)['answer'].strip()
+        if "кругл" in event_type:
+            return "круглый стол"
+        elif "индив" in event_type:
+            return "индивидуальная встреча"
+        elif "отрасл" in event_type:
+            return "отраслевая встреча"
+        elif "открыт" in event_type:
+            return "открытая встреча"
+        else:
+            return "индивидуальная встреча"
+
+
+    def parse_duration(self, text: str) -> int:
+        '''Поиск указания длительности встречи в запросе и парсинг в минуты'''
+        long = self.qa_model(question="Какова длительность события?", context=text)['answer'].strip()
+        numbers = ''.join(c if c.isdigit() else ' ' for c in long).split()
+        if "час" in long:
+            return int(numbers[0]) * 60
+        elif "мин" in long:
+            return int(numbers[0])
+        else:
+            return 30
+
+
+    def parse_date(self, text: str) -> datetime.datetime:
+        '''Поиск именованных сущностей дата/время в запросе и их парсинг в datetime'''
+        time = self.qa_model(question="О каком времени идет речь?", context=text)['answer'].strip()
+        time = ''.join(c if c.isdigit() else ' ' for c in time).split()
+        if len(time) == 1:
+            time.append('00')
+        date = self.qa_model(question="О какой дате идет речь?", context=text)['answer'].strip()
+        today = datetime.datetime.now()
+        if "сегодн" in date:
+            return parser.parse(f"{today.month} {today.day} {today.year} {time[0]}:{time[1]}")
+        elif "послезавтр" in date:
+            today_plus = (today + datetime.timedelta(days=2))
+            return parser.parse(f"{today_plus.month} {today_plus.day} {today_plus.year} {time[0]}:{time[1]}")
+        elif "завтр" in date:
+            today_plus = (today + datetime.timedelta(days=1))
+            return parser.parse(f"{today_plus.month} {today_plus.day} {today_plus.year} {time[0]}:{time[1]}")    
+        date = date.split()
+        try:
+            day = int(date[0])
+        except ValueError:
+            day = today.day
+        if len(date) > 1:        
+            month = self.transform_month(month=date[1])
+        else:
+            month = today.month
+        if len(date) > 2:
+            try:
+                year = int(date[2])
+            except ValueError:
+                pass
+        else:
+            year = today.year     
+        return parser.parse(f"{month} {day} {year} {time[0]}:{time[1]}")
+
+
+    def transform_month(self, month: str) -> int:
+        '''Перевод названий месяцев в их числовую форму'''
         if "январ" in month:
             return 1
         elif "феврал" in month:
             return 2
         elif "март" in month:
-            return 3        
+            return 3      
         elif "апрел" in month:
             return 4
         elif "ма" in month:
@@ -108,5 +146,5 @@ class Transcribator:
         elif "декабр" in month:
             return 12
         else:
-            return str(datetime.now().month)
+            return datetime.datetime.now().month
 
---
+++ src/transcribator_old.py
@@ -0,0 +1,112 @@
+import speech_recognition as sr
+from dateutil import parser
+from datetime import datetime
+import subprocess
+
+'''
+ВНИМАНИЕ!!! Тут все сделано максимально костыльно лишь для того,
+чтобы показать возможность и функциональность голосового управления.
+В случае согласования такого подхода это все будет переделано:
+
+- для транскрибации будет использоваться локальная модель (openAI Whisper large),
+чтобы аудио не уходило "налево" и для ускорения обработки;
+
+- для классификации типа обращения (запрос создания события, запрос удаления события,
+показ календаря и т.п.) будет использоваться специально созданная и обученная на
+пользовательских данных модель на архитектуре transformers;
+
+- для поиска именованных сущностей в тексте запроса (тип события, дата, время и т.п.)
+предполагается к использованию языковая модель NER (библиотека Spacy), также дообученная
+на пользовательских данных;
+
+- возможно, и для классификации, и для NER будет использоваться одна и та же модель на
+базе transformers;
+
+- также допускается, что для этих целей будет задействована коммерческая большая
+языковая модель-чатбот (вероятнее всего, gigachat от Сбера);
+
+Так как создание моделей это ОЧЕНЬ трудоемкий процесс, выполнить это за три дня
+не представилось возможным, поэтому демонстрируется лишь общая концепция.
+'''
+
+
+class Transcribator:
+    def __init__(self) -> None:
+        self.rec = sr.Recognizer()
+    
+    def transcribe(self, audiofile) -> list:
+        with sr.AudioFile(audiofile) as source:
+            audio = self.rec.record(source)
+            
+        try:
+            text = self.rec.recognize_google(audio, language="ru-RU")
+            return text.split()
+        
+        except sr.UnknownValueError:
+            print("Error: could not understand audio")
+            return ['Error']
+        except sr.RequestError as e:
+            print("Error; {0}".format(e))
+            return ['Error']
+    
+    def events_type_recognize(self, text: list) -> dict:
+        event = {}
+        if "покажи" in text:
+            if "сегодня" in text:
+                event['datetime'] = datetime.now()
+            if "неделю" in text:
+                event['datetime'] = datetime.now()
+            if "месяц" in text:
+                event['datetime'] = datetime.now()
+        elif "запланируй" in text:
+            event_datetime = self.keywords_finder(text=text)
+            event_datetime = parser.parse(f"{event_datetime['month']} {event_datetime['day']} {event_datetime['year']} {event_datetime['time']}")
+            event['event_type'] = text[text.index("запланируй") + 1]
+            event['place'] = text[text.index("место") + 1]  
+            event['datetime'] = event_datetime
+            return event
+        elif "отмени" in text:
+            event_datetime = self.keywords_finder(text=text)
+            event_datetime = parser.parse(f"{event_datetime['month']} {event_datetime['day']} {event_datetime['year']} {event_datetime['time']}")
+            event['event_type'] = text[text.index("отмени") + 1]
+            event['place'] = text[text.index("место") + 1]
+            event['datetime'] = event_datetime
+            return event
+
+    def keywords_finder(self, text: list) -> dict:
+        keywords = {}
+        keywords['time'] = text[text.index("время") + 1]
+        keywords['day'] = text[text.index("дата") + 1]
+        keywords['month'] = text[text.index("дата") + 2]
+        keywords['month'] = self.transform_month(keywords['month'])
+        keywords['year'] = text[text.index("дата") + 3]
+        return keywords
+
+    def transform_month(self, month: str) -> str:
+        if "январ" in month:
+            return 1
+        elif "феврал" in month:
+            return 2
+        elif "март" in month:
+            return 3        
+        elif "апрел" in month:
+            return 4
+        elif "ма" in month:
+            return 5
+        elif "июн" in month:
+            return 6
+        elif "июл" in month:
+            return 7
+        elif "август" in month:
+            return 8
+        elif "сентябр" in month:
+            return 9
+        elif "октябр" in month:
+            return 10
+        elif "ноябр" in month:
+            return 11
+        elif "декабр" in month:
+            return 12
+        else:
+            return str(datetime.now().month)
+
