aae618418dfed6a39d4728583e00c0ecc115d709
dkdrozdov <dkdrozdov@gmail.com>
2024-09-22 10:07:21 +0700

    hotfixes

--- src/nlp_requests.py
+++ src/nlp_requests.py
@@ -1,6 +1,7 @@
 from time import strftime
 from menu import ai_approval_menu
 from response import emptyCalendar_message, render_eventlist, valueError_message
+import settings
 from transcribator import Transcribator
 from scheduler import Scheduler
 
@@ -17,13 +18,26 @@ def search_action(tgbot, message, args):
     
     df = sc.show_day(year, month, day)
     
-    text = render_eventlist(df)
-    joint_text = '\n'.join(text)
+    events_on_day = settings.events.show_day(year, month, day)
     
-    if(not joint_text):
-        tgbot.send_message(chat_id=message.chat.id, text=emptyCalendar_message(day, month, year))
+    if not events_on_day.empty:
+        text = f"События на {year}-{month:02d}-{day:02d}:\n"
+        for _, event in events_on_day.iterrows():
+            text += f"- {event['event_name']} ({event['event_type']}) в {event['datetime'].strftime('%H:%M')}\n"
     else:
-        tgbot.send_message(chat_id=message.chat.id, text=joint_text)
+        text = f"На {year}-{month:02d}-{day:02d} нет запланированных событий."
+        
+    # tgbot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=text, reply_markup=call.message.reply_markup)
+    
+    tgbot.send_message(chat_id=message.chat.id, text=text)
+
+    # text = #render_eventlist(df)
+    # joint_text = '\n'.join(text)
+    
+    # if(not joint_text):
+    #     tgbot.send_message(chat_id=message.chat.id, text=emptyCalendar_message(day, month, year))
+    # else:
+    #     tgbot.send_message(chat_id=message.chat.id, text=joint_text)
     
     return
 
